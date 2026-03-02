bbe057f0d19306248b56de70f7c89f233b7e4b20
irineu <mmmirineusilva@gmail.com>
2026-03-01 22:01:28 -0300

    Feat(Categories): Add type tabs to category screen

--- composeApp/src/commonMain/kotlin/com/neoutils/finsight/ui/screen/categories/CategoriesAction.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/finsight/ui/screen/categories/CategoriesAction.kt
@@ -1,5 +1,8 @@
 package com.neoutils.finsight.ui.screen.categories
 
+import com.neoutils.finsight.domain.model.Category
+
 sealed class CategoriesAction {
     data object CreateDefaultCategories : CategoriesAction()
+    data class SelectType(val type: Category.Type) : CategoriesAction()
 }

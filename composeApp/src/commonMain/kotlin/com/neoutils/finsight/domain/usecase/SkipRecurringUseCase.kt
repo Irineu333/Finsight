bbe7a3d29985aab5d8e7ced7316d8644297451d5
irineu <mmmirineusilva@gmail.com>
2026-03-01 21:06:23 -0300

    Fix(Recurring): Use handled date month for confirm and skip

--- composeApp/src/commonMain/kotlin/com/neoutils/finsight/domain/usecase/SkipRecurringUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/finsight/domain/usecase/SkipRecurringUseCase.kt
@@ -6,16 +6,19 @@ import arrow.core.Either
 import arrow.core.Either.Companion.catch
 import com.neoutils.finsight.domain.model.Recurring
 import com.neoutils.finsight.domain.repository.IRecurringRepository
-import com.neoutils.finsight.extension.toYearMonth
-import kotlin.time.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.yearMonth
 import kotlin.time.ExperimentalTime
 
 class SkipRecurringUseCase(
     private val recurringRepository: IRecurringRepository,
 ) {
-    suspend operator fun invoke(recurring: Recurring): Either<Throwable, Unit> = catch {
+    suspend operator fun invoke(
+        recurring: Recurring,
+        date: LocalDate,
+    ): Either<Throwable, Unit> = catch {
         recurringRepository.update(
-            recurring.copy(lastHandledYearMonth = Clock.System.now().toYearMonth())
+            recurring.copy(lastHandledYearMonth = date.yearMonth)
         )
     }
 }

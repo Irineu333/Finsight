42420f98e0c6f88c4e135371a89073f99e3e2258
irineu <mmmirineusilva@gmail.com>
2026-03-11 21:58:54 -0300

    Revert: Remove "Print to HTML"

--- composeApp/src/commonMain/kotlin/com/neoutils/finsight/ui/screen/report/viewer/PrintReportAction.kt
+++
@@ -1,6 +0,0 @@
-package com.neoutils.finsight.ui.screen.report.viewer
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal expect fun rememberPrintReportAction(html: String, title: String): () -> Unit

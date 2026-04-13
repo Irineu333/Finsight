e32c8f47d68b0edc943ba88839883dca9d1586e4
irineu <mmmirineusilva@gmail.com>
2026-04-13 00:02:19 -0300

    Fix(UseCase): catch all exceptions from SetDefaultAccountUseCase

--- composeApp/src/commonMain/kotlin/com/neoutils/finsight/domain/usecase/SetDefaultAccountUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/finsight/domain/usecase/SetDefaultAccountUseCase.kt
@@ -2,23 +2,20 @@ package com.neoutils.finsight.domain.usecase
 
 import arrow.core.Either
 import arrow.core.Either.Companion.catch
-import arrow.core.raise.either
 import com.neoutils.finsight.domain.repository.IAccountRepository
 
 class SetDefaultAccountUseCase(
     private val repository: IAccountRepository
 ) {
-    suspend operator fun invoke(accountId: Long): Either<Throwable, Unit> = either {
+    suspend operator fun invoke(accountId: Long): Either<Throwable, Unit> = catch {
         val accounts = repository.getAllAccounts()
 
-        catch {
-            accounts.forEach { account ->
-                if (account.id == accountId && !account.isDefault) {
-                    repository.update(account.copy(isDefault = true))
-                } else if (account.id != accountId && account.isDefault) {
-                    repository.update(account.copy(isDefault = false))
-                }
+        accounts.forEach { account ->
+            if (account.id == accountId && !account.isDefault) {
+                repository.update(account.copy(isDefault = true))
+            } else if (account.id != accountId && account.isDefault) {
+                repository.update(account.copy(isDefault = false))
             }
-        }.bind()
+        }
     }
 }
